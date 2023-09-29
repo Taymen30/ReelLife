11c0b46a94c9448b2596ec12e8074c486276cb40
Taymen Verschuren <Taymen@iinet.net.au>
2023-09-29 08:48:32 +0800

    commented out

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.post('/login', (req, res) => {
                 console.log(`hello`, err)
                 if(bcrResult){
         
-                    req.session.userId = dbResult.rows[0].id
+                    // req.session.userId = dbResult.rows[0].id
                     return res.redirect('/')
                 } else{
                     return res.render('login')
